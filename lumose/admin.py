dc7de542ade3d58e5d7a10e45787617852ba85d8
Eric <erictaw2018@u.northwestern.edu>
2016-06-07 17:42:06 -0500

    'models'

--- lumose/admin.py
+++ lumose/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Photo, Tag
 
 # Register your models here.
+
+
+admin.site.register(Photo)
+admin.site.register(Tag)
--- lumose/models.py
+++ lumose/models.py
@@ -1,3 +1,27 @@
 from django.db import models
 
+
 # Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=40)
+    description = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Photo(models.Model):
+    title = models.CharField(max_length=80)
+    description = models.CharField(max_length=200)
+    file = models.FileField(upload_to='uploads/')
+    tags = models.ManyToManyField(Tag)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
