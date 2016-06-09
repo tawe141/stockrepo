cf761b9cf077204c38d85577586010f02b6a5fe7
Eric <erictaw2018@u.northwestern.edu>
2016-06-08 21:09:13 -0500

    uploads working

--- lumose/models.py
+++ lumose/models.py
@@ -1,4 +1,12 @@
 from django.db import models
+import storages.backends.s3boto
+
+
+protected_storage = storages.backends.s3boto.S3BotoStorage(
+    acl='private',
+    querystring_auth=True,
+    querystring_expire=600
+)
 
 
 # Create your models here.
@@ -16,7 +24,10 @@ class Tag(models.Model):
 class Photo(models.Model):
     title = models.CharField(max_length=80)
     description = models.CharField(max_length=200)
-    file = models.FileField(upload_to='uploads/')
+    file = models.FileField(
+        storage=protected_storage,
+        default='./lumose/static/imageNotFound.jpg'
+    )
     tags = models.ManyToManyField(Tag)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
--- lumose/views.py
+++ lumose/views.py
@@ -1,9 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import logging;
 
+
+logger = logging.getLogger(__name__)
 
 # Create your views here.
 
 
 def index(request):
     return HttpResponse("@index")
+
+
+def upload(request):
+    if request.method == 'GET':
+        return render(request, 'lumose/upload.html')
+    elif request.method == 'POST':
+        print(request.POST)
+        print(request.FILES)
+        return HttpResponse('received')
